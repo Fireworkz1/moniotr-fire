133f70c2c547dc8748ff12a4f52751dec6be2891
Sparks <smart030518@126.com>
2025-02-12 09:53:13 +0800

    prometheus的java api配置，非client的http访问和prometheus指标的解析 |bugfix

--- src/main/java/group/fire_monitor/prometheus/PrometheusQueryExecutor.java
+++ src/main/java/group/fire_monitor/prometheus/PrometheusQueryExecutor.java
@@ -1,30 +1,40 @@
 package group.fire_monitor.prometheus;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.net.URI;
+
 @Service
 public class PrometheusQueryExecutor {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String prometheusUrl = "http://8.130.20.137:9090/api/v1/query";
 
-    public PrometheusResponse executeQuery(String query) {
-        Map<String, String> params = new HashMap<>();
-        params.put("query", query);
-        try{
-            return PrometheusResponseParser.parse(restTemplate.getForObject(prometheusUrl, String.class, params));
-        }catch (Exception e){
+    public PrometheusResponse executeQuery(String query){
+
+        try {
+            String url = prometheusUrl + "?query={query}";
+            String ans=restTemplate.getForObject(url,String.class,query);
+            return PrometheusResponseParser.parse(ans);
+        } catch (Exception e) {
             e.printStackTrace();
             return new PrometheusResponse();
         }
 
     }
     public PrometheusResponse isMicroserviceOnline(String ip,String port){
-        String query = "up{instance=\""+ip+":"+port+"\"}";
+        String query = String.format("up{instance=\"%s:%s\"}", ip, port);
         return executeQuery(query);
     }
     public PrometheusResponse up()  {
